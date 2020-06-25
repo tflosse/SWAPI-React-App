4a8c69d29e2c84927130ecbfaae332a8d10e07e5
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-25 00:58:46 -0700

    Updates

---
+++ precache-manifest.e17a8164d4f587055d513026ecf54612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd34e571dba4def2595713391dd754ad",
+    "url": "/react-swapi/index.html"
+  },
+  {
+    "revision": "097b9d89e62deb7f9dc2",
+    "url": "/react-swapi/static/css/main.4d34b77f.chunk.css"
+  },
+  {
+    "revision": "4709f4938dcfd9b5b970",
+    "url": "/react-swapi/static/js/2.7cb96c5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-swapi/static/js/2.7cb96c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097b9d89e62deb7f9dc2",
+    "url": "/react-swapi/static/js/main.e1131d08.chunk.js"
+  },
+  {
+    "revision": "c43886207c27d8f69681",
+    "url": "/react-swapi/static/js/runtime-main.a44742a2.js"
+  }
+]);
